51e6945d8a48ee04f47150829d6017eb8d16aeb5
YarikBur <yarikbur@yandex.ru>
2023-12-21 21:57:31 +0300

    Add parser texture tileset

--- core/src/ru/yarikbur/test/utils/TilesetParser.java
+++ core/src/ru/yarikbur/test/utils/TilesetParser.java
@@ -1,5 +1,21 @@
 package ru.yarikbur.test.utils;
 
-public class TilesetParser {
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
+public class TilesetParser {
+	private TextureRegion[][] tileset;
+	
+	/**
+	 * @param col - column in texture tileset
+	 * @param row - row in texture tileset
+	 * @param tex - texture tileset
+	 */
+	public TilesetParser(int col, int row, Texture tex) {
+		tileset = TextureRegion.split(tex, tex.getWidth() / col, tex.getHeight() / row);
+	}
+	
+	public TextureRegion[][] getTileset() {
+		return tileset;
+	}
 }

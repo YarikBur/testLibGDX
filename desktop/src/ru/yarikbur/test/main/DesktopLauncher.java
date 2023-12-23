8f947e264c9e35246d9a50ffbe328e1fb8b54357
YarikBur <yarikbur@yandex.ru>
2023-12-23 20:52:16 +0300

    Apply a window settings

--- desktop/src/ru/yarikbur/test/main/DesktopLauncher.java
+++ desktop/src/ru/yarikbur/test/main/DesktopLauncher.java
@@ -2,14 +2,17 @@ package ru.yarikbur.test.main;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
 import ru.yarikbur.test.game.main.Main;
+import ru.yarikbur.test.settings.GlobalSettings;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setForegroundFPS(60);
+		config.setForegroundFPS(GlobalSettings.FOREGROUND_FPS);
 		config.setTitle("Test GDX");
+		config.setWindowedMode(GlobalSettings.WINDOW_WIDTH, GlobalSettings.WINDOW_HEIGHT);
 		new Lwjgl3Application(new Main(), config);
 	}
 }
